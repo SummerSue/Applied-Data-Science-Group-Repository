10c38eddbfa200577e5f1a01cf5e3ffa76349f3e
Sam Gass <sdg2145@columbia.edu>
2017-04-03 18:15:17 -0400

    zri bug fixed

--- Final Project/Creating Panel Data.R
+++ Final Project/Creating Panel Data.R
@@ -1,36 +1,41 @@
+chicago <- read.csv("chicagolistings.csv")
+chicago_zri <- filter(zri, City == "Chicago")
+neigh_chicago <- unique(chicago_zri$RegionName)
+chicago$host_since <- as.Date(chicago$host_since)
 
-austin_bnb <- data.frame()
+chicago_bnb <- data.frame()
 aird <- data.frame()
 air <- data.frame()
-for (i in neigh_aus){
+for (i in neigh_chicago){
   aird <- data.frame()
   air <- data.frame()
   for (k in dat){ 
-    hood <- filter(austin, neighbourhood == i) 
+    hood <- filter(chicago, neighbourhood_cleansed == i) 
     aird[1,1] <- i
     aird[1,2] <- as.data.frame(nrow(filter(hood, host_since <= as.Date(k))))
     aird[1,3] <- as.character(k)
     air <- rbind(aird, air)
   }
-  austin_bnb <- rbind(air, austin_bnb)
+  chicago_bnb <- rbind(air, chicago_bnb)
 }
 
-austin_bnb$city <- "Austin"
+chicago_bnb$City <- "chicago"
 
 #Getting Zillow Data in
 library(reshape)
-austin_zri <- filter(zri, City == "Austin")
-austin_zri <- austin_zri[,-c(1,3:7)]
-austin_zri <- melt(austin_zri)
-austin_zri <- as.data.frame(sapply(austin_zri, function(x) gsub("X", "", x)))
-austin_zri$variable <- as.character(austin_zri$variable)
-austin_zri$RegionName <- as.character(austin_zri$RegionName)
-austin_zri$value <- as.numeric(austin_zri$value)
-austin_zri$variable <- lapply(austin_zri$variable, function(x) paste(x,"01", sep="."))
-austin_zri$variable <- as.character(austin_zri$variable)
-austin_zri$variable <- as.Date(austin_zri$variable, "%Y.%m.%d")
-colnames(austin_zri) <- c("Neighborhood", "Date", "ZRI")
-colnames(austin_bnb) <- c("Neighborhood", "Airbnb", "Date", "City")
-austin_bnb$Date <- as.Date(austin_bnb$Date)
 
-test <- left_join(austin_bnb, austin_zri, by = c("Neighborhood", "Date"))
+chicago_zri <- chicago_zri[,-c(1,3:7)]
+chicago_zri <- melt(chicago_zri)
+chicago_zri <- as.data.frame(sapply(chicago_zri, function(x) gsub("X", "", x)))
+chicago_zri$variable <- as.character(chicago_zri$variable)
+chicago_zri$RegionName <- as.character(chicago_zri$RegionName)
+chicago_zri$value <- as.numeric(as.character(chicago_zri$value))
+chicago_zri$variable <- lapply(chicago_zri$variable, function(x) paste(x,"01", sep="."))
+chicago_zri$variable <- as.character(chicago_zri$variable)
+chicago_zri$variable <- as.Date(chicago_zri$variable, "%Y.%m.%d")
+colnames(chicago_zri) <- c("Neighborhood", "Date", "ZRI")
+colnames(chicago_bnb) <- c("Neighborhood", "Airbnb", "Date", "City")
+chicago_bnb$Date <- as.Date(chicago_bnb$Date)
+
+chicago_bnb <- left_join(chicago_bnb, chicago_zri, by = c("Neighborhood", "Date"))
+chicago_bnb <- filter(chicago_bnb, Date <= as.Date("2017-02-01"))
